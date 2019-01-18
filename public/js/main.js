a3e3189f0b49d8f5e135638d911ed3acc680ed35
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-01-18 20:56:21 +0000

    complete redesign - added pages, animations, etc

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,55 @@
 $(function(){
+	$.fn.riseIn = function(milli, cb){
+		milli = milli || 400;
+		if (this.css("display") == "none" || this.css("opacity") == "0") {
+			this
+			.css({
+				display: "block",
+				opacity: 0,
+				marginTop: "50px"
+			})
+			.animate({
+				marginTop: 0,
+				opacity: 1
+			}, milli, function() {
+				$(this).css({
+					marginTop: ""
+				});
+				if (cb) cb();
+			});
+		}
+		return this;
+	}
+
 	$(".menu").click(function(){
 		$("nav .link-group").slideDown(null, "easeOutExpo");
 	});
 
 	$(".close-menu").click(function(){
-		$("nav .link-group").slideUp(null, "easeOutExpo", function() {
+		$("nav .link-group").slideUp(null, "easeInExpo", function() {
 			$(this).css("display", "");
 		});
 	});
 
+	$("nav .link").click(function(e){
+		if (window.innerWidth < 768 && $(this).hasClass("expand")) {
+			e.preventDefault();
+			$("." + this.id + "-sublist").stop().slideToggle();
+		}
+	})
+
 	// bg auto-playing slideshow
-	$("#bgreel > div:gt(0)").hide();
+	$("#index-reel > div:gt(0)").hide();
 	setInterval(function() {
-		$('#bgreel > div:first')
-			.delay(1000)
-			.fadeOut(2000)
+		$('#index-reel > div:first')
 			.next()
-			.fadeIn(2000)
+			.show()
 			.end()
-			.appendTo('#bgreel');
-	}, 5000);
+			.fadeOut(2000)
+			.appendTo('#index-reel');
+	}, 4000);
+
+	$(".products .thumb").each(function(i) {
+		$(this).delay(i * 200).riseIn(1000)
+	});
 });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render('index');
+	res.render('index')
+});
+
+router.get('/products', (req, res) => {
+	res.render('products', {
+		clothingSection: [
+			[
+
+				{ imgSrc: 'https://assets.bigcartel.com/product_images/224256073/3AA09521-E79A-4ACC-B38C-79FADCEE4F61.jpeg?auto=format&fit=max&w=900', 
+				productName: 'WBR Classic Tee (White)',
+				price: "13.50" },
+
+				{ imgSrc: 'https://assets.bigcartel.com/product_images/224256073/3AA09521-E79A-4ACC-B38C-79FADCEE4F61.jpeg?auto=format&fit=max&w=900', 
+				productName: 'WBR Classic Tee (White)',
+				price: "13.50" },
+
+				{ imgSrc: 'https://assets.bigcartel.com/product_images/224256073/3AA09521-E79A-4ACC-B38C-79FADCEE4F61.jpeg?auto=format&fit=max&w=900', 
+				productName: 'WBR Classic Tee (White)',
+				price: "13.50" }
+
+			], [
+
+				{ imgSrc: 'https://assets.bigcartel.com/product_images/224256073/3AA09521-E79A-4ACC-B38C-79FADCEE4F61.jpeg?auto=format&fit=max&w=900', 
+				productName: 'WBR Classic Tee (White)',
+				price: "13.50" },
+
+				{ imgSrc: 'https://assets.bigcartel.com/product_images/224256073/3AA09521-E79A-4ACC-B38C-79FADCEE4F61.jpeg?auto=format&fit=max&w=900', 
+				productName: 'WBR Classic Tee (White)',
+				price: "13.50" }
+
+			]
+		]
+	});
 });
 
 module.exports = router;
