d42c3d6f032ed1436f9a6c18d5e92c5de8e18b2b
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-01-21 00:49:38 +0000

    amendments for '/shopify/callback' GET request

--- routes/shopify.js
+++ routes/shopify.js
@@ -20,13 +20,13 @@ const apiSecret = process.env.SHOPIFY_API_SECRET || s3.SHOPIFY_API_SECRET;
 const scopes = 'write_products';
 
 // replacing actual forwarding address
-const forwardingAddress = "http://werentbornrich.herokuapp.com";
+const forwardingAddress = "https://werentbornrich.herokuapp.com";
 
 
 router.get('/', (req, res) => {
 	const shop = req.query.shop;
 	if (shop) {
-		const state = nonce();
+		const state = nonce()();
 		const redirectUri = forwardingAddress + "/shopify/callback";
 		const installUrl = "https://" + shop + "/admin/oauth/authorize?client_id=" + apiKey +
 		"&scope=" + scopes +
@@ -46,32 +46,26 @@ router.get("/callback", (req, res) => {
 	const stateCookie = cookie.parse(req.headers.cookie).state;
 
 	if (state !== stateCookie) {
-		return res.status(403).send('Request origin cannot be verified');
+		return res.status(403).send({
+			msg: 'Request origin cannot be verified',
+			state: state,
+			stateCookie: stateCookie,
+			"typeof state": typeof state,
+			"typeof stateCookie": typeof stateCookie
+		});
 	}
 
 	if (shop && hmac && code) {
 		// DONE: Validate request is from Shopify
 		const map = Object.assign({}, req.query);
-		delete map['signature'];
 		delete map['hmac'];
 		const message = querystring.stringify(map);
-		const providedHmac = Buffer.from(hmac, 'utf-8');
-		const generatedHash = Buffer.from(
-			crypto
-				.createHmac('sha256', apiSecret)
-				.update(message)
-				.digest('hex'),
-				'utf-8'
-			);
-		let hashEquals = false;
+		const generatedHash = crypto
+			.createHmac('sha256', apiSecret)
+			.update(message)
+			.digest('hex');
 
-		try {
-			hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
-		} catch (e) {
-			hashEquals = false;
-		};
-
-		if (!hashEquals) {
+		if (generatedHash !== hmac) {
 			return res.status(400).send('HMAC validation failed');
 		}
 
@@ -80,27 +74,27 @@ router.get("/callback", (req, res) => {
 		const accessTokenPayload = {
 			client_id: apiKey,
 			client_secret: apiSecret,
-			code,
+			code
 		};
 
 		request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-		.then((accessTokenResponse) => {
+		.then(accessTokenResponse => {
 			const accessToken = accessTokenResponse.access_token;
 			// DONE: Use access token to make API call to 'shop' endpoint
 			const shopRequestUrl = 'https://' + shop + '/admin/shop.json';
-			const shopRequestHeaders = {
-				'X-Shopify-Access-Token': accessToken,
+			const shopRequestHeader = {
+				'X-Shopify-Access-Token': accessToken
 			};
 
-			request.get(shopRequestUrl, { headers: shopRequestHeaders })
-			.then((shopResponse) => {
-				res.status(200).end(shopResponse);
+			request.get(shopRequestUrl, { headers: shopRequestHeader })
+			.then(apiResponse => {
+				res.status(200).end(apiResponse);
 			})
-			.catch((error) => {
+			.catch(error => {
 				res.status(error.statusCode).send(error.error.error_description);
 			});
 		})
-		.catch((error) => {
+		.catch(error => {
 			res.status(error.statusCode).send(error.error.error_description);
 		});
 
