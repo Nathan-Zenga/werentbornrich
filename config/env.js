43c8d6a31eba088f96201e2b546cf9c7c521f4cb
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-02-12 04:21:09 +0000

    adding env variables for nodemailer

--- config/env.js
+++ config/env.js
@@ -7,13 +7,17 @@ module.exports = function() {
 	s3 = new aws.S3({
 		apiKey: process.env.SHOPIFY_API_KEY,
 		apiSecret: process.env.SHOPIFY_API_SECRET,
-		access_token: process.env.SHOPIFY_ACCESS_TOKEN
+		access_token: process.env.SHOPIFY_ACCESS_TOKEN,
+		authUser: process.env.NODEMAILER_AUTH_EMAIL,
+		authPass: process.env.NODEMAILER_AUTH_PASS
 	});
 
 	const variables = {
 		apiKey: process.env.SHOPIFY_API_KEY,
 		apiSecret: process.env.SHOPIFY_API_SECRET,
-		access_token: process.env.SHOPIFY_ACCESS_TOKEN
+		access_token: process.env.SHOPIFY_ACCESS_TOKEN,
+		authUser: process.env.NODEMAILER_AUTH_EMAIL,
+		authPass: process.env.NODEMAILER_AUTH_PASS
 	};
 
 	return variables
