6a7784a86776fe7916ca9e93a8934163c160591a
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-02-20 17:53:31 +0000

    disassociating callback from displaying form results

--- config/show-products-callback.js
+++ config/show-products-callback.js
@@ -6,35 +6,22 @@ module.exports = (req, res, next) => {
 	Shopify().get("/admin/products.json", null, function(err, data) {
 		if (err) return res.send(err);
 
-		let arr;
-		let headingTitle;
+		let typeParam = req.params.product;
+		let filteredList = [];
 
-		if (!req.session.searchResults && !req.session.input) {
-			let typeParam = req.params.product;
-			let filteredList = [];
-
-			if (typeParam) {
-				data.products.forEach(product => {
-					if (typeParam.includes(product.product_type)) filteredList.push(product);
-				});
-			}
-
-			arr = filteredList.length ? filteredList : data.products;
-			headingTitle = typeParam && filteredList.length ? typeParam : req.originalUrl === "/products" ? "Products" : null;
-		} else {
-			arr = req.session.searchResults;
-			headingTitle = "Search Results: " + req.session.input;
+		if (typeParam) {
+			data.products.forEach(product => {
+				if (typeParam.includes(product.product_type)) filteredList.push(product);
+			});
 		}
+
+		let arr = filteredList.length ? filteredList : data.products;
+		let headingTitle = typeParam && filteredList.length ? typeParam : req.originalUrl === "/products" ? "Products" : null;
 
 		if (headingTitle) {
 			res.render("products", {
 				headingTitle: headingTitle,
 				products: splitPerRow(arr, 3)
-			}, (err, html) => {
-				if (err) return err;
-				req.session.searchResults = undefined;
-				req.session.input = undefined;
-				res.send(html);
 			})
 		} else {
 			next()
