1c552134c72142b86f7214e35ea6ed61f54e2a8c
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-02-23 00:53:12 +0000

    destroying session after checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -5,26 +5,25 @@ const stripe = require("stripe")(env.stripeSK);
 
 router.post("/charge", (req, res, next) => {
 	const amount = req.body.amount;
+	const callback = (err, charge) => req.session.destroy(err => res.render("success", { headingTitle: "Checkout" }));
 	
-	stripe.customers.create({
-		email: req.body.stripeEmail,
-		source: req.body.stripeToken
-	})
-	.then(customer => {
-		stripe.charges.create({
-			amount,
-			description: "WBR Purchase",
-			currency: "gbp",
-			customer: customer.id
-		});
-	})
-	.then((err, charge) => res.redirect("/checkout/success"));
-});
-
-router.get("/success", (req, res, next) => {
-	res.render("success", {
-		headingTitle: "Checkout"
-	})
+	if (req.session.items && req.session.items.length) {
+		stripe.customers.create({
+			email: req.body.stripeEmail,
+			source: req.body.stripeToken
+		})
+		.then(customer => {
+			stripe.charges.create({
+				amount,
+				description: "WBR Purchase",
+				currency: "gbp",
+				customer: customer.id
+			});
+		})
+		.then(callback);
+	} else {
+		callback();
+	}
 });
 
 module.exports = router;
